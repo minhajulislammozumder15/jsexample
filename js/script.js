8f99023e295fcea8f7af042e2d37cfb81cca4f84
Minhajul Islam Mozumder <154677361+minhajulislammozumder15@users.noreply.github.com>
2024-03-15 22:35:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,74 @@
+
+function textChange(){
+   document.getElementById('t_c').innerHTML= "hello";
+}
+
+function showDate(){
+   document.getElementById('s_d').innerHTML= Date();
+}
+function canChange(){
+   document.getElementById('c_x').innerHTML= "Hello JavaScript!";
+}
+
+function bulbOn(){
+	document.getElementById('bulb').src='image/on.gif';
+}
+
+function bulbOff(){
+	document.getElementById('bulb').src='image/off.gif';
+}
+
+function colorChange(){
+	document.getElementById('c_c').style.color="yellow";
+}
+function colorChange1(){
+	document.getElementById('c_c1').style.color="green";
+}
+function colorChange2(){
+	document.getElementById('c_c2').style.color="blue";
+}
+function textShow(){
+	document.getElementById('demo').style.display='none'
+}
+function textHide(){
+	document.getElementById('demo').style.display='block'
+}
+
+function window.print(){
+	document.getElementById('w_p').innerHTML="print";
+}
+function changeTime(){
+	document.getElementById("t_y").innerHTML= Date();
+}
+
+ /*function bulbOn(){
+	document.getElementById('bulb').src='image/on.gif';
+}
+
+function bulbOff(){
+	document.getElementById('bulb').src='image/off.gif';
+}
+
+function textShow(){
+	document.getElementById('demo').style.display='none'
+}
+function textHide(){
+	document.getElementById('demo').style.display='block'
+}
+
+function textChanged(){
+	document.getElementById('text').innerHTML= "paragrapg changed";
+}
+
+
+function numChanged(){
+	document.getElementById('textnum').innerHTML= 5+6;
+}
+
+function numberChanged(){
+	document.getElementById('textnumber').innerHTML=5+6;
+}
+
+function (){
+	document.getElementById('dolly').innerHTML= "hello dolly";
+} */
